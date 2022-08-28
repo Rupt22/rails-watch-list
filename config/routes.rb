73714d1fc92ae439047fe39728e58656893992c9
rupert sealey <rupertsealey@hotmail.com>
2022-08-28 14:03:25 +0100

    some broken CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists, only: %i[index show create new] do
+  resources :lists, only: %i[index show create new destroy] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: :destroy
