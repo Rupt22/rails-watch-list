5ca42ee3fbd5a1786af9c8b79878ba134efb5be4
Rupert Sealey <rupertsealey@hotmail.com>
2022-10-19 12:01:34 +0100

    validations added to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :list
+
+  validates :description, presence: true, length: {minimum: 10}
+  validates :rating, presence: true, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 10}
+
 end
